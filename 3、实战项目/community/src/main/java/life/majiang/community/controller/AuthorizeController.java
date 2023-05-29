c213e9578890079da0bc6c6a0dc2c610aabc36c1
huyihao <1123176978@qq.com>
2023-05-29 17:18:19 +0800

    实战项目 - 论坛 - 2、使用Thymeleaf在页面显示Github OAuth登录的用户信息

--- 3、实战项目/community/src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ 3、实战项目/community/src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Github OAuth登录回调
  */
@@ -34,7 +36,8 @@ public class AuthorizeController {
       */
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -43,7 +46,14 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getGithubUser(accessToken);
         log.info("user: " + user);
-        return "index";
+        if (user != null) {
+            // 登录成功，写cookie、session
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            // 登录失败，重新登录
+            return "redirect:/";
+        }
     }
 
 }
